35839459201e81c97deb5b93d93a13d38ef37ac4
3x3cut0r <julianreith@gmx.de>
2025-07-21 15:34:31 +0200

    merge pr-57 till pr-68

--- build/lib/ImageHorizonLibrary/recognition/_screenshot.py
+++ build/lib/ImageHorizonLibrary/recognition/_screenshot.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from os.path import abspath, relpath, join as path_join
+from os.path import abspath, join as path_join
 from random import choice
 from string import ascii_lowercase
 
@@ -13,17 +13,18 @@ from ..errors import ScreenshotFolderException
 class _Screenshot(object):
     def _make_up_filename(self):
         try:
-            path = BuiltIn().get_variable_value("${SUITE NAME}")
-            path = "%s-screenshot" % path.replace(" ", "")
+            path = BuiltIn().get_variable_value('${SUITE NAME}')
+            path = '%s-screenshot' % path.replace(' ', '')
         except RobotNotRunningError:
-            LOGGER.info("Could not get suite name, using " "default naming scheme")
-            path = "ImageHorizon-screenshot"
-        path = "%s-%d.png" % (path, self.screenshot_counter)
+            LOGGER.info('Could not get suite name, using '
+                        'default naming scheme')
+            path = 'ImageHorizon-screenshot'
+        path = '%s-%d.png' % (path, self.screenshot_counter)
         self.screenshot_counter += 1
         return path
 
     def take_a_screenshot(self):
-        """Takes a screenshot of the screen.
+        '''Takes a screenshot of the screen.
 
         This keyword is run on failure if it is not overwritten when
         `importing` the library.
@@ -35,19 +36,13 @@ class _Screenshot(object):
         running integer appended. If this keyword is used outside of Robot
         Framework execution, file name is this library's name with running
         integer appended.
-        """
-        target_dir = self.screenshot_folder if self.screenshot_folder else ""
+        '''
+        target_dir = self.screenshot_folder if self.screenshot_folder else ''
         if not isinstance(target_dir, str):
-            raise ScreenshotFolderException(
-                "Screenshot folder is invalid: " '"%s"' % target_dir
-            )
+            raise ScreenshotFolderException('Screenshot folder is invalid: '
+                                            '"%s"' % target_dir)
         path = self._make_up_filename()
         path = abspath(path_join(target_dir, path))
-        logpath = BuiltIn().get_variable_value("${OUTPUT DIR}")
-        relativepath = relpath(path, start=logpath).replace("\\", "\/")
-        LOGGER.info(
-            'Screenshot taken: {0}<br/><img src="{0}" '
-            'width="100%" />'.format(relativepath),
-            html=True,
-        )
+        LOGGER.info('Screenshot taken: {0}<br/><img src="{0}" '
+                    'width="100%" />'.format(path), html=True)
         ag.screenshot(path)
